c7b50f5014c9ed359b8f990b017904ec45aad08f
Bokshyts, Yauheni <yauheni.bokshits@thomsonreiters.com>
2019-11-06 04:55:18 +0300

    small refactor for module_1

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
 
 const env = process.env.NODE_ENV || 'development';
 const isDev = env === 'development';
@@ -12,32 +13,33 @@ const extractScss = new ExtractTextPlugin({
 });
 
 module.exports = {
-  entry: {
-    bundle: './src/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-   },
-	plugins: [
-	 new HtmlWebpackPlugin(),
-	 extractScss
-	],
-   module: {
-     rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: 'babel-loader'
-    }, {
-       test: /(\.css|\.scss)$/,
-       exclude: /node_modules/,
-	   use: extractScss.extract({
-        use:[
-          {loader: 'css-loader'},
-          {loader: 'sass-loader'}
+        entry: {
+          bundle: './src/index.js'
+        },
+        output: {
+          path: path.resolve(__dirname, 'dist'),
+          filename: 'bundle.js'
+        },
+        devtool: 'inline-source-map',
+        plugins: [
+          new HtmlWebpackPlugin(),
+          extractScss
         ],
-       fallback: 'style-loader'
-     })
-	}]
-  }
+        module: {
+          rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+          }, {
+            test: /(\.css|\.scss)$/,
+            exclude: /node_modules/,
+          use: extractScss.extract({
+              use:[
+                {loader: 'css-loader'},
+                {loader: 'sass-loader'}
+              ],
+            fallback: 'style-loader'
+          })
+        }]
+    }
 };
